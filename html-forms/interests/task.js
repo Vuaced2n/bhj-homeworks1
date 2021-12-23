acf1957be58bc475863178fcd97f4db4fd3b23d3
Aleksey Zayachnikov <lekseff@gmail.com>
2021-12-24 01:20:25 +0300

    сохранеине

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -4,41 +4,49 @@ class CheckBox {
   constructor(container) {
     this.container = container;
     this.primaryCheckBox = this.container.querySelector('.interest__check');
+    this.secondCheckBox = this.container.querySelector('ul.interests');
+    this.secondCheckItems = this.secondCheckBox.querySelectorAll('input[type="checkbox"]')
 
     this.registerEvents();
-
-    this.log();
   }
 
   registerEvents() {
-    this.primaryCheckBox.addEventListener('change', this.log.bind(this));
+    this.primaryCheckBox.addEventListener('change', () => {
+      this.checkPrimary();
+    });
+
+    this.secondCheckItems.forEach(elem => {
+      elem.addEventListener('change', this.checkSecond.bind(this));
+    });
   }
 
-  log() {
-    const item = 1;
-    const subCheckBox = this.container.querySelector('.interests_active');
-    const subCheckBoxItems = subCheckBox.querySelectorAll('input');
-    console.log(subCheckBoxItems);
-
-    subCheckBoxItems.forEach(elem => {
+  checkPrimary() {
+    this.secondCheckBox.querySelectorAll('input[type="checkbox"]').forEach(elem => {
       elem.checked = this.primaryCheckBox.checked;
-    })
-    
-    // if(this.primaryCheckBox.checked) {
-    //   console.log('+');
-    //   subCheckBoxItems.forEach(elem => {
-    //     elem.checked = true;
-    //   })
-    //
-    // } else {
-    //   console.log('-');
-    // }
+    });
+  }
 
-    // console.log(this.primaryCheckBox);
+  checkSecond() {
+    console.log('checkSecond');
 
+    const activeCheckValue = this.secondCheckBox.querySelectorAll('input[type="checkbox"]:checked').length;
+
+
+    if (activeCheckValue > 0 && activeCheckValue < this.secondCheckItems.length) {
+      this.primaryCheckBox.indeterminate = true;
+    } else if (activeCheckValue === this.secondCheckItems.length) {
+      this.primaryCheckBox.indeterminate = false;
+      this.primaryCheckBox.checked = true;
+    } else {
+      this.primaryCheckBox.indeterminate = false;
+      this.primaryCheckBox.checked = false;
+    }
   }
 
 }
 
 
-new CheckBox(document.querySelectorAll('ul li.interest')[0]);
+const Box1 = new CheckBox(document.querySelectorAll('.interests_main > ul > li')[0]);
+const Box2 = new CheckBox(document.querySelectorAll('.interests_main > ul > li')[1]);
+
+// console.log(document.querySelectorAll('.interests_main > ul > li')[1])
