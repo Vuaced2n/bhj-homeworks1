fd2fbe6e71e562591a6dc30b1b1b16b682226e47
Aleksey Zayachnikov <lekseff@gmail.com>
2021-12-24 21:46:42 +0300

    сохранение

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -3,50 +3,90 @@
 class CheckBox {
   constructor(container) {
     this.container = container;
-    this.primaryCheckBox = this.container.querySelector('.interest__check');
-    this.secondCheckBox = this.container.querySelector('ul.interests');
-    this.secondCheckItems = this.secondCheckBox.querySelectorAll('input[type="checkbox"]')
+
+    this.checkBoxAll = this.container.querySelectorAll('input[type="checkbox"]');
+
 
     this.registerEvents();
   }
 
   registerEvents() {
-    this.primaryCheckBox.addEventListener('change', () => {
-      this.checkPrimary();
+    this.checkBoxAll.forEach(elem => {
+      elem.addEventListener('change', this.checkPrimary.bind(this))
     });
 
-    this.secondCheckItems.forEach(elem => {
-      elem.addEventListener('change', this.checkSecond.bind(this));
-    });
+
   }
 
-  checkPrimary() {
-    this.secondCheckBox.querySelectorAll('input[type="checkbox"]').forEach(elem => {
-      elem.checked = this.primaryCheckBox.checked;
-    });
-  }
+  checkPrimary(event) {
+    console.log(this.checkBoxAll);
 
-  checkSecond() {
-    console.log('checkSecond');
+    // Текущий элемент
+    const currentElement = event.target;
+    console.log('Текущий эл: ', currentElement);
 
-    const activeCheckValue = this.secondCheckBox.querySelectorAll('input[type="checkbox"]:checked').length;
+    //наличие родителя
+    let subCheckBoxAll = currentElement.closest('ul.interests');
+    console.log('наличие родителя список: ', subCheckBoxAll);
 
+    // let parentCheckBox;
 
-    if (activeCheckValue > 0 && activeCheckValue < this.secondCheckItems.length) {
-      this.primaryCheckBox.indeterminate = true;
-    } else if (activeCheckValue === this.secondCheckItems.length) {
-      this.primaryCheckBox.indeterminate = false;
-      this.primaryCheckBox.checked = true;
+    let parentCheckBox;
+
+    if (subCheckBoxAll === null) {
+      subCheckBoxAll = currentElement.closest('li.interest');
+      parentCheckBox = currentElement;
     } else {
-      this.primaryCheckBox.indeterminate = false;
-      this.primaryCheckBox.checked = false;
+      parentCheckBox = subCheckBoxAll.previousElementSibling.querySelector('input');
     }
+    
+
+
+    // const parentCheckBox = subCheckBoxAll.previousElementSibling.querySelector('input');
+    //Предыдущий главный чекбокс
+    console.log('Главный чек: ', parentCheckBox);
+
+
+    // Всего элементов в подсписке
+    const allCheckboxValue = subCheckBoxAll.children.length;
+    console.log('Всего элементов в подсписке: ', allCheckboxValue);
+
+    // Отмечено элементов
+    const activeCheckboxValue = subCheckBoxAll.querySelectorAll('input[type=checkbox]:checked').length;
+    console.log('Отмечено элементов: ', activeCheckboxValue);
+
+    if (activeCheckboxValue === allCheckboxValue) {
+      parentCheckBox.indeterminate = false;
+      parentCheckBox.checked = true;
+    } else if (activeCheckboxValue > 0 && activeCheckboxValue < allCheckboxValue) {
+      parentCheckBox.indeterminate = true;
+    } else {
+      parentCheckBox.indeterminate = false;
+      parentCheckBox.checked = false;
+    }
+
+    
+    console.log('наличие доп списка', Boolean(currentElement.closest('li').querySelector('ul')));
+    
+    if (Boolean(currentElement.closest('li').querySelector('ul'))) {
+      
+      const allSubCheckbox = currentElement.closest('li').querySelectorAll('ul > li');
+      console.log('доп список', allSubCheckbox);
+      
+      allSubCheckbox.forEach(elem => {
+        // console.log(elem)
+        elem.querySelector('input').checked = currentElement.checked;
+      })
+    }
+
   }
+
 
 }
 
 
 const Box1 = new CheckBox(document.querySelectorAll('.interests_main > ul > li')[0]);
+
 const Box2 = new CheckBox(document.querySelectorAll('.interests_main > ul > li')[1]);
 
-// console.log(document.querySelectorAll('.interests_main > ul > li')[1])
+// console.log(document.querySelectorAll('.interests_main > ul > li')[0]);
