d899384c5c268166de111e8a78bd7220f7fccf98
Aleksey Zayachnikov <lekseff@gmail.com>
2021-12-27 14:04:36 +0300

    html-forms complete

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -3,90 +3,41 @@
 class CheckBox {
   constructor(container) {
     this.container = container;
-
-    this.checkBoxAll = this.container.querySelectorAll('input[type="checkbox"]');
-
+    this.mainCheckbox = this.container.firstElementChild.querySelector('input');
+    this.subCheckbox = this.container.querySelectorAll('ul.interests input');
 
     this.registerEvents();
+
   }
 
   registerEvents() {
-    this.checkBoxAll.forEach(elem => {
-      elem.addEventListener('change', this.checkPrimary.bind(this))
+    this.mainCheckbox.addEventListener('change', this.allChecked.bind(this));
+    this.subCheckbox.forEach(elem => {
+      elem.addEventListener('change', this.selectCheckbox.bind(this))
     });
-
-
   }
 
-  checkPrimary(event) {
-    console.log(this.checkBoxAll);
-
-    // Текущий элемент
-    const currentElement = event.target;
-    console.log('Текущий эл: ', currentElement);
-
-    //наличие родителя
-    let subCheckBoxAll = currentElement.closest('ul.interests');
-    console.log('наличие родителя список: ', subCheckBoxAll);
-
-    // let parentCheckBox;
-
-    let parentCheckBox;
-
-    if (subCheckBoxAll === null) {
-      subCheckBoxAll = currentElement.closest('li.interest');
-      parentCheckBox = currentElement;
-    } else {
-      parentCheckBox = subCheckBoxAll.previousElementSibling.querySelector('input');
-    }
-    
-
-
-    // const parentCheckBox = subCheckBoxAll.previousElementSibling.querySelector('input');
-    //Предыдущий главный чекбокс
-    console.log('Главный чек: ', parentCheckBox);
-
-
-    // Всего элементов в подсписке
-    const allCheckboxValue = subCheckBoxAll.children.length;
-    console.log('Всего элементов в подсписке: ', allCheckboxValue);
-
-    // Отмечено элементов
-    const activeCheckboxValue = subCheckBoxAll.querySelectorAll('input[type=checkbox]:checked').length;
-    console.log('Отмечено элементов: ', activeCheckboxValue);
-
-    if (activeCheckboxValue === allCheckboxValue) {
-      parentCheckBox.indeterminate = false;
-      parentCheckBox.checked = true;
-    } else if (activeCheckboxValue > 0 && activeCheckboxValue < allCheckboxValue) {
-      parentCheckBox.indeterminate = true;
-    } else {
-      parentCheckBox.indeterminate = false;
-      parentCheckBox.checked = false;
-    }
-
-    
-    console.log('наличие доп списка', Boolean(currentElement.closest('li').querySelector('ul')));
-    
-    if (Boolean(currentElement.closest('li').querySelector('ul'))) {
-      
-      const allSubCheckbox = currentElement.closest('li').querySelectorAll('ul > li');
-      console.log('доп список', allSubCheckbox);
-      
-      allSubCheckbox.forEach(elem => {
-        // console.log(elem)
-        elem.querySelector('input').checked = currentElement.checked;
-      })
-    }
-
+  allChecked() {
+    this.subCheckbox.forEach(elem => {
+      elem.checked = this.mainCheckbox.checked;
+    });
   }
 
+  selectCheckbox() {
+    const activeValueCheckbox = this.container.querySelectorAll('ul.interests input[type="checkbox"]:checked').length;
+    const allCheckbox = this.subCheckbox.length;
 
+    if (activeValueCheckbox === allCheckbox) {
+      this.mainCheckbox.indeterminate = false;
+      this.mainCheckbox.checked = true;
+    } else if (activeValueCheckbox > 0 && activeValueCheckbox < allCheckbox) {
+      this.mainCheckbox.indeterminate = true;
+    } else if (activeValueCheckbox === 0) {
+      this.mainCheckbox.indeterminate = false;
+      this.mainCheckbox.checked = false;
+    }
+  }
 }
 
-
-const Box1 = new CheckBox(document.querySelectorAll('.interests_main > ul > li')[0]);
-
-const Box2 = new CheckBox(document.querySelectorAll('.interests_main > ul > li')[1]);
-
-// console.log(document.querySelectorAll('.interests_main > ul > li')[0]);
+new CheckBox(document.querySelectorAll('.interests_main > ul > li')[0]);
+new CheckBox(document.querySelectorAll('.interests_main > ul > li')[1]);
