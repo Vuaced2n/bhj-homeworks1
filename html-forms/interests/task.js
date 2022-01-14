59324a6c9b610c7e8048d5279c99e766e839126d
Aleksey Zayachnikov <lekseff@gmail.com>
2022-01-14 23:12:22 +0300

    checkbox complete

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,43 +1,69 @@
 'use strict'
 
-class CheckBox {
-  constructor(container) {
-    this.container = container;
-    this.mainCheckbox = this.container.firstElementChild.querySelector('input');
-    this.subCheckbox = this.container.querySelectorAll('ul.interests input');
+const checkList = document.querySelector('ul')
+const checkItems = checkList.querySelectorAll('input[type="checkbox"]')
 
-    this.registerEvents();
+checkItems.forEach(elem => {
+  elem.addEventListener('click', clickHandler)
+})
 
+function clickHandler(event) {
+  const activeElement = event.target
+  const childElement = activeElement.closest('li.interest').querySelector('ul.interests')
+
+  if (childElement) {
+    const checkboxItems = childElement.querySelectorAll('input[type="checkbox"]')
+    checkboxItems.forEach(elem => {
+      elem.checked = activeElement.checked
+      elem.indeterminate = false
+    })
   }
 
-  registerEvents() {
-    this.mainCheckbox.addEventListener('change', this.allChecked.bind(this));
-    this.subCheckbox.forEach(elem => {
-      elem.addEventListener('change', this.selectCheckbox.bind(this))
-    });
-  }
-
-  allChecked() {
-    this.subCheckbox.forEach(elem => {
-      elem.checked = this.mainCheckbox.checked;
-    });
-  }
-
-  selectCheckbox() {
-    const activeValueCheckbox = this.container.querySelectorAll('ul.interests input[type="checkbox"]:checked').length;
-    const allCheckbox = this.subCheckbox.length;
-
-    if (activeValueCheckbox === allCheckbox) {
-      this.mainCheckbox.indeterminate = false;
-      this.mainCheckbox.checked = true;
-    } else if (activeValueCheckbox > 0 && activeValueCheckbox < allCheckbox) {
-      this.mainCheckbox.indeterminate = true;
-    } else if (activeValueCheckbox === 0) {
-      this.mainCheckbox.indeterminate = false;
-      this.mainCheckbox.checked = false;
-    }
-  }
+  checkParent(activeElement)
 }
 
-new CheckBox(document.querySelectorAll('.interests_main > ul > li')[0]);
-new CheckBox(document.querySelectorAll('.interests_main > ul > li')[1]);
+
+/**
+ * Проверка родителя и чек
+ * @param elem
+ */
+function checkParent(elem) {
+
+  //Родитель активного элемента
+  const parentActiveElement = elem.closest('ul.interests_active')
+
+  //Если родительского элемента нет выходим
+  if (parentActiveElement == null) return
+
+
+  //Получаем родителя активного чекбокса
+  const parentActiveCheckbox = parentActiveElement.closest('li.interest').querySelector('input[type="checkbox"]')
+  // Массив дочерних элементов
+  const checkboxItems = Array.from(parentActiveElement.children)
+
+
+  if (checkboxItems.every(elem => {
+    const checkboxItem = elem.querySelector('input[type="checkbox"]')
+    return checkboxItem.checked && checkboxItem.indeterminate === false
+  })) {
+    //Отмечены все
+    parentActiveCheckbox.indeterminate = false
+    parentActiveCheckbox.checked = true
+  } else if (checkboxItems.some(elem => {
+    return elem.querySelector('input[type="checkbox"]').checked
+  })) {
+    //Отмечен хотя бы один
+    parentActiveCheckbox.indeterminate = true
+  } else if (checkboxItems.some(elem => {
+    return elem.querySelector('input[type="checkbox"]').indeterminate === true
+  })) {
+    //Отмеченн хотя бы один indeterminate
+    parentActiveCheckbox.indeterminate = true
+  } else {
+    // Не отмеченно ни одного
+    parentActiveCheckbox.indeterminate = false
+    parentActiveCheckbox.checked = false
+  }
+
+  checkParent(parentActiveCheckbox)
+}
